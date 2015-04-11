b1faca52fb46d0711032b9dc0bc7f9684f07b474
RafaelUL <rafaelulima@gmail.com>
2015-04-11 12:32:20 -0400

    Now it's actually the final version

--- AI/flaskAI.py
+++ AI/flaskAI.py
@@ -1,6 +1,9 @@
 import json
 import re
 import random
+import urllib2
+import pyglet
+import os
 
 from flask import Flask
 app = Flask(__name__)
@@ -11,7 +14,7 @@ def save_to_file(stt):
 	markovM = gen_markov(linesM.values())
 
 	t = generate(stt, markovM, linesM)
-	print t
+	get_audio(t)
 
 if __name__ == '__main__':
 	app.run(host='10.122.1.106', port=33334)
@@ -133,3 +136,40 @@ def generate(input_str, markov, lines):
 			rand_val -= value
 			
 	return output_str
+
+''' Request audio from text. Save to file '''
+def get_audio(text):
+	formatted_text = urllib2.quote(text)
+
+	audio_url = urllib2.urlopen(BASE_URL + formatted_text).geturl()
+	audio_data = urllib2.urlopen(audio_url).read()
+
+	audio_file = open(AUDIO_FILENAME, 'w')
+	audio_file.write(audio_data)
+	audio_file.flush()
+	audio_file.close()
+
+''' Play audio that was already requested and saved '''
+def play_audio():
+	audio = pyglet.resource.media(AUDIO_FILENAME)
+	audio.play()
+	pyglet.clock.schedule_once(exiter, audio.duration)
+	pyglet.app.run()
+
+''' Stops pyglet from hanging after sound has played '''
+def exiter(dt):
+	pyglet.app.exit()
+
+#BASE_URL = 'http://tts-api.com/tts.mp3?q='
+#AUDIO_FILENAME = 'audio.mp3'
+
+#pyglet.resource.path = [os.getcwd()]
+#pyglet.resource.reindex()
+
+#linesM = get_lines("macbeth.txt", "MACBETH")
+#markovM = gen_markov(linesM.values())
+
+#t = generate("good evening", markovM, linesM)
+#with open("Output.txt", "w") as text_file:
+#	text_file.write(t)
+#get_audio(t)

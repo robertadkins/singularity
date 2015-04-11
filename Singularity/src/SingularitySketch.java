091270f7296a86fc132ed39b20079cbe75adb5a7
RafaelUL <rafaelulima@gmail.com>
2015-04-11 12:02:12 -0400

    rockin' that face tracker

--- Singularity/src/SingularitySketch.java
+++ Singularity/src/SingularitySketch.java
@@ -13,29 +13,37 @@ public class SingularitySketch extends PApplet {
 
 	private CascadeClassifier face_cascade;
 
-	float normX;
-	float normY;
-	float prevNormX;
-	float prevNormY;
+	private float DAMP = 0.2f;
+	private float HEAD_ANGLE_RANGE = PI / 8;
+	private float EYE_ANGLE_RANGE = PI / 5;
+
+	float targetX;
+	float targetY;
+	float prevX;
+	float prevY;
 
 	PShape s;
 	PShape eye;
-	float angleX = 15 * PI / 8;
-	float angleY = 0;
+	
+	float headAngleX = 15 * PI / 8;
+	float headAngleY = 0;
+	
+	float eyeAngleX = PI / 70;
+	float eyeAngleY = 0;
 
 	public void setup() {
-		size(600, 600, P3D);
+		size(1200, 900, P3D);
 		s = loadShape("my_model.obj");
 		eye = loadShape("my_eye.obj");
 
-		normX = 0.5f;
-		normY = 0.5f;
-		prevNormX = 0.5f;
-		prevNormY = 0.5f;
+		targetX = 0.5f;
+		targetY = 0.5f;
+		prevX = 0.5f;
+		prevY = 0.5f;
 
 		System.loadLibrary("opencv_java2411");
 		face_cascade = new CascadeClassifier(
-				"C:/Users/Rafael/Downloads/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml");
+				"data/haarcascade_frontalface_default.xml");
 		if (face_cascade.empty()) {
 			System.out.println("--(!)Error loading A\n");
 			return;
@@ -46,39 +54,59 @@ public class SingularitySketch extends PApplet {
 		spiderman.start();
 	}
 
-	public void draw(){
-	  background(0x000000);
-	    
-	  
-	  lights();
-	//  directionalLight(50,255,50,0,-1,0);
-	  pushMatrix();
-	  translate(width/2, height/2, 0);
-	  scale(2.0f);
-	  angleY += (prevNormX - normX) * PI / 2;
-	  prevNormX = normX;
-	  angleX += (prevNormY - normY) * PI / 2;
-	  prevNormY = normY;
-	  rotateY(angleY);
-	  rotateX(angleX);
-	  rotateZ(PI);
-	  shape(s);
-	  pushMatrix();
-	  translate(-36, 36.5f, 28);
-	  rotateY(angleY);
-	  rotateX(angleX);
-	  scale(22.5f);
-	  shape(eye);
-	  popMatrix();
-	  translate(36, 36.5f, 28);
-	  rotateY(angleY);
-	  rotateX(angleX);
-	  scale(22.5f);
-	  shape(eye);
-	  camera(0, 0, 1000,
-	         0  , 0  , 0   ,
-	         0  , 1  , 0    );
-	  popMatrix();
+	public void draw() {
+		background(0x000000);
+		lights();
+		//directionalLight(50,255,50,0,-1,0);
+		pushMatrix();
+		translate(width / 2, height / 2, 0);
+		scale(3.0f);
+		
+		float dx = prevX - targetX;
+		if (abs(dx) > 0.001) {
+			headAngleY += dx * DAMP * HEAD_ANGLE_RANGE;
+			prevX -= dx * DAMP;
+		}
+		
+		float dy = prevY - targetY;
+		if (abs(dy) > 0.001) {
+			headAngleX += dy * DAMP * HEAD_ANGLE_RANGE;
+			prevY -= dy * DAMP;
+		}
+		
+		rotateX(headAngleX);
+		rotateY(headAngleY);
+		rotateZ(PI);
+		shape(s);
+		
+		dx = prevX - targetX;
+		if (abs(dx) > 0.001) {
+			eyeAngleY += dx * DAMP * EYE_ANGLE_RANGE;
+			prevX -= dx * DAMP;
+		}
+		
+		dy = prevY - targetY;
+		if (abs(dy) > 0.001) {
+			eyeAngleX += dy * DAMP * EYE_ANGLE_RANGE;
+			prevY -= dy * DAMP;
+		} 
+		
+		pushMatrix();
+		translate(-36, 36.5f, 28);
+		rotateY(-eyeAngleY);
+		rotateX(-eyeAngleX);
+		scale(22.5f);
+		shape(eye);
+		popMatrix();
+		
+		translate(36, 36.5f, 28);
+		rotateY(-eyeAngleY);
+		rotateX(-eyeAngleX);
+		scale(22.5f);
+		shape(eye);
+		camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
+		
+		popMatrix();
 	}
 
 	class Processor implements Runnable {
@@ -100,7 +128,7 @@ public class SingularitySketch extends PApplet {
 			Imgproc.cvtColor(mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
 			Imgproc.equalizeHist(mGrey, mGrey);
 			face_cascade.detectMultiScale(mGrey, faces);
-			Rect best = new Rect();
+			Rect best = null;
 			int maxArea = 0;
 			for (Rect rect : faces.toArray()) {
 				if (rect.width * rect.height > maxArea) {
@@ -108,9 +136,12 @@ public class SingularitySketch extends PApplet {
 					best = rect;
 				}
 			}
-			normX = (float) (best.x / inputframe.size().width);
-			normY = (float) (best.y / inputframe.size().height);
-
+			
+			if (best != null && maxArea > 15000) {
+				targetX = (float) (best.x / inputframe.size().width);
+				targetY = (float) (best.y / inputframe.size().height);
+			}
+			
 			return mRgba;
 		}
 

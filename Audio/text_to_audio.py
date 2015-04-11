903258ed39382b431c293d996c65fc2c598ffe15
RafaelUL <rafaelulima@gmail.com>
2015-04-11 02:35:02 -0400

    Some code refactoring for text_to_audio. Doesn't hang anymore.

--- Audio/text_to_audio.py
+++ Audio/text_to_audio.py
@@ -8,18 +8,31 @@ AUDIO_FILENAME = 'audio.mp3'
 pyglet.resource.path = [os.getcwd()]
 pyglet.resource.reindex()
 
-print os.getcwd()
+''' Request audio from text. Save to file '''
+def get_audio(text):
+    formatted_text = urllib2.quote(text)
 
-text = raw_input()
-formatted_text = urllib2.quote(text)
+    audio_url = urllib2.urlopen(BASE_URL + formatted_text).geturl()
+    audio_data = urllib2.urlopen(audio_url).read()
 
-audio_url = urllib2.urlopen(BASE_URL + formatted_text).geturl()
-audio_data = urllib2.urlopen(audio_url).read()
+    audio_file = open(AUDIO_FILENAME, 'w')
+    audio_file.write(audio_data)
+    audio_file.flush()
+    audio_file.close()
 
-audio_file = open(AUDIO_FILENAME, 'w')
-audio_file.write(audio_data)
-audio_file.close()
+''' Play audio that was already requested and saved '''
+def play_audio():
+    audio = pyglet.resource.media(AUDIO_FILENAME)
+    audio.play()
+    pyglet.clock.schedule_once(exiter, audio.duration)
+    pyglet.app.run()
 
-audio = pyglet.resource.media(AUDIO_FILENAME)
-audio.play()
-pyglet.app.run()
+''' Stops pyglet from hanging after sound has played '''
+def exiter(dt):
+    pyglet.app.exit()
+
+get_audio(raw_input())
+play_audio()
+
+
+

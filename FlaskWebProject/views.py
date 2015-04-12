fd47331d82903fb00c4534300192f0c092f25ea9
RafaelUL <rafaelulima@gmail.com>
2015-04-12 00:23:48 -0400

    back to working locally. html updated

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -23,7 +23,7 @@ def save_to_file(stt):
     t = generate(stt, markovM, linesM)
     get_audio(t)
     sock = socket.socket()
-    sock.connect((request.remote_addr, 115))
+    sock.connect((request.remote_addr, 33333))
     sock.send(open(AUDIO_FILENAME, 'rb').read())
     sock.close()
     return t
